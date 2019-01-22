456d10f2c3534d44a7a19df2e12d448c4cc58186
nhanthiennguyen <nhanthiennguyen@kms-technology.com>
2019-01-22 17:15:27 +0700

    #Change project to JS

---
+++ src/old/app/App.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import Login from './components/login/Login';
+
+export default class App extends Component {
+  render() {
+    return <Login />;
+  }
+}
---
+++ src/old/app/routes/AppRouter.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+// import Login from 'components/login/Login';
+// import SelectTool from 'components/select/select_tool/SelectTool';
+
+// // import ManageUsers from
+
+// export default class AppRouter extends Component {
+//   render() {
+//     return (
+//       <Router>
+//         <div>
+//           <Route component={Login} />
+//           <Route component={SelectTool} />
+//         </div>
+//       </Router>
+//     );
+//   }
+// }
